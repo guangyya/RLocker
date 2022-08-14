fdf7f0be431d3ff85f5dd32dece8f4dfbd57b9b8
guangyya <guangyya@cisco.com>
2022-08-14 10:26:35 +0800

    Initial Commit

---
+++ RLocker/RLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RLockerApp.swift
+//  RLocker
+//
+//  Created by Guangyu Yang on 2022/8/14.
+//
+
+import SwiftUI
+
+@main
+struct RLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
